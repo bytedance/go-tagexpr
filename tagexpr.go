0b281c5e2c1f8e0db92470c88447b2da3ac61012
henrylee2cn <henrylee2cn@gmail.com>
2019-01-22 20:52:59 +0800

    feat: update tagexpr.go
    Change-Id: Ibbdf2ae22fe6b909112c0fb1960e640b80bb019c

--- tagexpr.go
+++ tagexpr.go
@@ -1,13 +1,49 @@
 package tagexpr
 
-// TagExpr struct tag expression
-type TagExpr struct {
+// TagExpr struct tag expression evaluator
+type TagExpr interface {
+	// Eval evaluate the value of the struct tag expression by the selector expression.
+	// format: fieldName.exprName, fieldName1.fieldName2.exprName1
+	Eval(selector string) interface{}
+	// Range loop through each tag expression
+	Range(func(selector string, eval func() interface{}))
+}
+
+// Struct struct tag expression
+type Struct struct {
+	name    string
+	fields  map[string]*Field
+	exprLib Lib
+}
+
+// Lib tag expression lib
+// key format: fieldName.exprName, fieldName1.fieldName2.exprName1
+type Lib map[string]*Expr
+
+// Eval evaluate the value of the struct tag expression by the selector expression.
+// format: fieldName.exprName, fieldName1.fieldName2.exprName1
+func (t Lib) Eval(selector string) interface{} {
+	return nil
+}
+
+// Range loop through each tag expression
+func (t Lib) Range(func(selector string, eval func() interface{})) {
+
+}
+
+type Field struct {
+	name   string
+	host   *Struct
+	sub    *Struct // struct field
+	offset uintptr
 }
 
 // New creates a struct tag expression
-func New(structPtr interface{}) *TagExpr {
-	t := &TagExpr{}
+func New(structPtr interface{}) *Struct {
+	t := &Struct{}
 	return t
 }
 
-// func
+func (s *Struct) Run(structPtr interface{}) TagExpr {
+	return nil
+}
