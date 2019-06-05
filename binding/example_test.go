d8922d9546af20222df751de684294cf19518340
henrylee2cn <henrylee2cn@gmail.com>
2019-06-06 01:27:11 +0800

    style(binding): Update example
    Change-Id: I075695a881f0ab82a7b642e8a5123f471f136ac8

--- binding/example_test.go
+++ binding/example_test.go
@@ -1,9 +1,11 @@
 package binding_test
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"strings"
 
 	"github.com/bytedance/go-tagexpr/binding"
 	"github.com/henrylee2cn/goutil/httpbody"
@@ -15,26 +17,43 @@ func Example() {
 		Year          []int    `api:"{query:'year'}"`
 		Email         *string  `api:"{body:'email'}{@:email($)}"`
 		Friendly      bool     `api:"{body:'friendly'}"`
-		Pie           float32  `api:"{body:'pie'}"`
+		Pie           float32  `api:"{body:'pie'}{required:true}"`
 		Hobby         []string `api:"{body:'hobby'}"`
+		BodyNotFound  *int     `api:"{body:'xxx'}"`
 		Authorization string   `api:"{header:'Authorization'}{required:true}{@:$=='Basic 123456'}"`
 		SessionID     string   `api:"{cookie:'sessionid'}{required:true}"`
 		AutoBody      string
 		AutoQuery     string
-		NotFound      *string
+		AutoNotFound  *string
 	}
 
 	args := new(InfoRequest)
 	binder := binding.New("api")
 	err := binder.BindAndValidate(args, requestExample(), new(testPathParams))
 
-	fmt.Printf("bind and validate error: %v\n", err)
+	fmt.Println("bind and validate result:")
+
+	fmt.Printf("error: %v\n", err)
 
 	b, _ := json.MarshalIndent(args, "", "	")
 	fmt.Printf("args JSON string:\n%s\n", b)
 
 	// Output:
-	// bind and validate error: <nil>
+	// request:
+	// POST /?year=2018&year=2019&AutoQuery=autoquery_test HTTP/1.1
+	// Host: localhost
+	// User-Agent: Go-http-client/1.1
+	// Transfer-Encoding: chunked
+	// Authorization: Basic 123456
+	// Content-Type: application/json;charset=utf-8
+	// Cookie: sessionid=987654
+	//
+	// 83
+	// {"AutoBody":"autobody_test","email":"henrylee2cn@gmail.com","friendly":true,"hobby":["Coding","Mountain climbing"],"pie":3.1415926}
+	// 0
+	//
+	// bind and validate result:
+	// error: <nil>
 	// args JSON string:
 	// {
 	// 	"Name": "henrylee2cn",
@@ -49,11 +68,12 @@ func Example() {
 	// 		"Coding",
 	// 		"Mountain climbing"
 	// 	],
+	// 	"BodyNotFound": null,
 	// 	"Authorization": "Basic 123456",
 	// 	"SessionID": "987654",
 	// 	"AutoBody": "autobody_test",
 	// 	"AutoQuery": "autoquery_test",
-	// 	"NotFound": null
+	// 	"AutoNotFound": null
 	// }
 }
 
@@ -71,5 +91,12 @@ func requestExample() *http.Request {
 	cookies := []*http.Cookie{
 		{Name: "sessionid", Value: "987654"},
 	}
-	return newRequest("http://localhost/?year=2018&year=2019&AutoQuery=autoquery_test", header, cookies, bodyReader)
+	req := newRequest("http://localhost/?year=2018&year=2019&AutoQuery=autoquery_test", header, cookies, bodyReader)
+	req.Method = "POST"
+	var w bytes.Buffer
+	req.Write(&w)
+	fmt.Printf("request:\n%s", strings.Replace(w.String(), "\r\n", "\n", -1))
+
+	bodyReader.(*bytes.Reader).Seek(0, 0)
+	return req
 }
