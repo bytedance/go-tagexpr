3c3e845064ec5ac9e6bc5bed8a17cf8180bf447d
henrylee2cn <henrylee2cn@gmail.com>
2021-10-28 12:57:39 +0800

    chore(binding): support Content-Encoding
    Change-Id: I8ff72eb62108bcd5a7418e434d87cbe03a8fbf37

--- binding/body.go
+++ binding/body.go
@@ -2,6 +2,9 @@ package binding
 
 import (
 	"bytes"
+	"compress/flate"
+	"compress/gzip"
+	"compress/zlib"
 	jsonpkg "encoding/json"
 	"errors"
 	"io"
@@ -60,12 +63,30 @@ func GetBody(r *http.Request) (*Body, error) {
 		body.Reset()
 		return body, nil
 	default:
-		var buf bytes.Buffer
-		_, err := io.Copy(&buf, r.Body)
-		r.Body.Close()
+		var err error
+		var closeBody = r.Body.Close
+		switch r.Header.Get("Content-Encoding") {
+		case "gzip":
+			var gzipReader *gzip.Reader
+			gzipReader, err = gzip.NewReader(r.Body)
+			if err == nil {
+				r.Body = gzipReader
+			}
+		case "deflate":
+			r.Body = flate.NewReader(r.Body)
+		case "zlib":
+			var readCloser io.ReadCloser
+			readCloser, err = zlib.NewReader(r.Body)
+			if err == nil {
+				r.Body = readCloser
+			}
+		}
 		if err != nil {
 			return nil, err
 		}
+		var buf bytes.Buffer
+		_, _ = io.Copy(&buf, r.Body)
+		_ = closeBody()
 		_body := &Body{
 			Buffer:    &buf,
 			bodyBytes: buf.Bytes(),
