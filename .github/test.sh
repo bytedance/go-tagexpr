ba75e9af8f87fb3d3420a6b0cdbb3ba0ca869ac8
henrylee2cn <henrylee2cn@gmail.com>
2022-06-09 11:33:40 +0800

    fix: test.sh
    Change-Id: I51c3fb2356361ddcca23d236e32bb853904d2658

--- .github/test.sh
+++ .github/test.sh
@@ -14,7 +14,7 @@ echo 'mode: atomic' > coverage.txt
 for ele in "${module_list[@]}";
 do
   echo "start handle sub_module: $ele"
-  go test -covermode=atomic -coverprofile=coverage.tmp -coverpkg=./... -parallel 1 -p 1 -count=1 -gcflags=-l $ele | rerun-fail-test -retry-times=5 -- -covermode=atomic -coverprofile=coverage.tmp -coverpkg=./... -parallel 1 -p 1 -count=1 -gcflags=-l $ele
+  go test -covermode=atomic -coverprofile=coverage.tmp -coverpkg=./... -parallel 1 -p 1 -count=1 -gcflags=-l $ele
   tail -n +2 coverage.tmp >> coverage.txt || echo ""
   rm coverage.tmp || echo ""
 done
